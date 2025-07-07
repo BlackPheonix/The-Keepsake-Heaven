d6515dcac429d092e9967d0d565bf10ee51fbb4d
Dulneth Perera <136075091+BlackPheonix@users.noreply.github.com>
2025-07-08 00:09:36 +0530

    Delete src directory

--- src/pages/Login.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const Login = () => (
-  <div style={{ padding: "3rem 0", textAlign: "center" }}>
-    <h2>Login / Signup</h2>
-    <p>This is a placeholder for the Login/Signup form.</p>
-  </div>
-);
-export default Login;
--- src/pages/Register.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const Register = () => (
-  <div style={{ padding: "3rem 0", textAlign: "center" }}>
-    <h2>Register</h2>
-    <p>This is a placeholder for the Register form.</p>
-  </div>
-);
-export default Register;
